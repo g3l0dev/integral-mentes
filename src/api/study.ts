d6d8e8ffa999efff4118adb2bcf614571b950a48
Angelo94a <angelo94.aa@gmail.com>
2024-04-01 23:18:44 -0500

    actualizado

---
+++ src/api/study.ts
@@ -0,0 +1,22 @@
+export const STUDIES = [
+  {
+    title: "USB Cartagena",
+    body:"Psicóloga"
+  },
+  {
+    title: "Unisinu Cartagena",
+    body:"Especialista en Psicología Clínica de Niños y Adolescentes"
+  },
+  {
+    title:"Universidad Tecnológica Internacional",
+    body:"Master en Psicoterapia Cognitivo-Conductual"
+  },
+  {
+    title: "Universidad Javeriana",
+    body:"Docencia Universitaria"
+  },
+  {
+    title: "Universidad de los Andes",
+    body:"Primeros auxilios Psicológicos"
+  }
+]
