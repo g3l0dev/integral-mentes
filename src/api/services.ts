79ba29d8cce1c920ad8edd943cc99d7afdd4eba3
Angelo94a <angelo94.aa@gmail.com>
2024-04-01 21:39:17 -0500

    actualizando

--- src/api/services.ts
+++ src/api/services.ts
@@ -1,5 +1,20 @@
-export const SERVICES = [
+export const EXPERIENCE = [
   {
-    
-  }
-]
+    texto: "Trastornos del Neurodesarrollo",
+  },
+  {
+    texto: "Trastornos del estado de ánimo",
+  },
+  {
+    texto: "Problemas de Autoconcepto y Autoestima",
+  },
+  {
+    texto: "Problemas de aprendizaje",
+  },
+  {
+    texto: "Desregulación Emocional",
+  },
+  {
+    texto: "Ejecución de Proyectos Psicosociales",
+  },
+];
