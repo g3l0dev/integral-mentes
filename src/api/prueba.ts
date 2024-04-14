6116ffa437391352b4685ecc3675d446da168559
Angelo94a <angelo94.aa@gmail.com>
2024-04-14 07:23:03 -0500

    actualizando

---
+++ src/api/prueba.ts
@@ -0,0 +1,8 @@
+export const PRUEBA = [
+  {
+    title: "Prueba 1",
+    body: "esto es una prueba para la extensión de auto completado compilot.",
+    id: 1,
+    
+  }
+]
---
+++ src/api/tarifa.ts
@@ -0,0 +1,16 @@
+export const TARIFA = [
+  {
+    title: 'Cartagena',
+    inicial: '160.000',
+    sesion: '90.000',
+    sesiones: '500.000'
+  },
+  
+  {
+    title: 'Soplaviento',
+    inicial: '60.000',
+    sesion: '50.000',
+    sesiones: '260.000'
+  },
+  
+]
